96968078f148eaf8faf474d16d23b039df0edd1f
Karl Cereno <karlcereno@gmail.com>
2021-10-14 23:13:47 -0400

    First commit. Create skeleton component layout

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header">
+          <h1>header</h1>
+        </div>
+        <div className="timer">
+          <h1>timer</h1>
+        </div>
+        <div className="tracker">
+          <h1>tracker</h1>
+        </div>
+        <div className="footer">
+          <h1>footer</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
